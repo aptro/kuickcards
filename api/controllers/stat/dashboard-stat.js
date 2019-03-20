31a063f90046d27b5350d33d3372bb59036fd1c5
akash <akashkupatro@gmail.com>
2019-03-20 14:49:27 +0530

    stats year change bug fix

--- api/controllers/stat/dashboard-stat.js
+++ api/controllers/stat/dashboard-stat.js
@@ -80,31 +80,91 @@ module.exports = {
     var count_remember_true = await sails.sendNativeQuery(query_remember_true, [year, this.req.user.id]);
     var count_remember_false = await sails.sendNativeQuery(query_remember_false, [year, this.req.user.id]);
     var count_card_added = await sails.sendNativeQuery(query_card_added, [year, this.req.user.id]);
-    var locals = {
+    var data = {
       remember_true: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       remember_false: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      card_added: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      card_added: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     };
+
     _.forEach(labels, function (l, index) {
       _.forEach(count_remember_true.rows, function (rt) {
         if (l == rt.month)
-          locals.remember_true[index] = parseInt(rt.count);
+          data.remember_true[index] = parseInt(rt.count);
       });
       _.forEach(count_remember_false.rows, function (rt) {
         if (l == rt.month)
-          locals.remember_false[index] = parseInt(rt.count);
+          data.remember_false[index] = parseInt(rt.count);
       });
       _.forEach(count_card_added.rows, function (rt) {
         if (l == rt.month)
-          locals.card_added[index] = parseInt(rt.count);
+          data.card_added[index] = parseInt(rt.count);
       });
-    })
+    });
+
+    var locals = {
+      //…
+      filter: year,
+      chart_config: {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            data: data.remember_true,
+            label: 'Remember',
+            backgroundColor: "rgba(153, 102, 255, 0.7)",
+            borderColor: "rgb(153, 102, 255)",
+            fill: true
+          }, {
+            data: data.remember_false,
+            label: 'Not Remember',
+            backgroundColor: "rgba(75, 192, 192, 0.7)",
+            borderColor: "rgb(75, 192, 192)",
+            fill: true
+          }, {
+            data: data.card_added,
+            label: 'Cards Added',
+            backgroundColor: "rgba(255, 159, 64, 0.7)",
+            borderColor: "rgb(255, 159, 64)",
+            fill: true
+          }]
+        },
+        options: {
+          responsive: true,
+          title: {
+            display: true,
+            text: 'Stats'
+          },
+          tooltips: {
+            mode: 'index',
+            intersect: false,
+          },
+          hover: {
+            mode: 'nearest',
+            intersect: true
+          },
+          scales: {
+            xAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Month'
+              }
+            }],
+            yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Count'
+              }
+            }]
+          }
+        }
+      }
+    }
 
     if (this.req.query.json == 'true')
       return exits.success(locals);
     return exits.view(locals);
 
   }
-
-
 };
--- assets/js/pages/stats.page.js
+++ assets/js/pages/stats.page.js
@@ -1,79 +1,8 @@
-window.chartColors = {
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
-};
-var color = Chart.helpers.color;
-
 parasails.registerPage('stats', {
   //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
-    filter: "<%=req.query.filter%>",
-    cards_learned: 0,
-    total_cards: 0,
-    chart_config: {
-      type: 'line',
-      data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [{
-          data: SAILS_LOCALS.remember_true,
-          label: 'Remember',
-          backgroundColor: color(window.chartColors.purple).alpha(0.7).rgbString(),
-          borderColor: window.chartColors.purple,
-          fill: true
-        }, {
-          data: SAILS_LOCALS.remember_false,
-          label: 'Not Remember',
-          backgroundColor: color(window.chartColors.green).alpha(0.7).rgbString(),
-          borderColor: window.chartColors.green,
-          fill: true
-        }, {
-          data: SAILS_LOCALS.card_added,
-          label: 'Cards Added',
-          backgroundColor: color(window.chartColors.orange).alpha(0.7).rgbString(),
-          borderColor: window.chartColors.orange,
-          fill: true
-        }]
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Stats'
-        },
-        tooltips: {
-          mode: 'index',
-          intersect: false,
-        },
-        hover: {
-          mode: 'nearest',
-          intersect: true
-        },
-        scales: {
-          xAxes: [{
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            }
-          }],
-          yAxes: [{
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Count'
-            }
-          }]
-        }
-      }
-    }
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -81,12 +10,11 @@ parasails.registerPage('stats', {
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function () {
     // Attach any initial data from the server.
-    this.
-      _.extend(this, SAILS_LOCALS);
+    _.extend(this, SAILS_LOCALS);
   },
   mounted: async function () {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    window.myLine = new Chart(ctx, this.chart_config);
+    this.ctx = document.getElementById('canvas').getContext('2d');
+    window.myLine = new Chart(this.ctx, this.chart_config);
     if (canvas.height < 400) {
       window.myLine.options.legend.display = false;
       window.myLine.update();
@@ -107,11 +35,12 @@ parasails.registerPage('stats', {
       $('.inverted.dimmer').addClass('active');
       var _this = this;
       Cloud['dashboardStat'].with({ filter: this.filter, json: true }).exec(function (err, q) {
+        _this.chart_config = q.chart_config;
+        _this.filter = q.filter;
+        window.myLine.data = _this.chart_config.data;
+        window.myLine.update()
         $('.inverted.dimmer').removeClass('active');
-        _this.cards_learned = q.cards_learned;
-        _this.total_cards = q.total_cards;
-
-      })
+      });
     }
   }
 });
